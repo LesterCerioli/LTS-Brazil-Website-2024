af022525d8c2d85cc59c9ccd39361e009752e4ab
DouglasRibeiro1972 <douglascri@yahoo.com.br>
2023-08-15 16:46:08 -0300

    issue(#938): styling contact

--- src/components/contact/form/form.tsx
+++ src/components/contact/form/form.tsx
@@ -1,38 +1,58 @@
-import React from 'react'
-import * as S from './styles'
+import React from "react";
+import * as S from "./styles";
 
 const Form = () => {
   return (
     <S.Form>
-    <S.Title>
-        <h1>Fale conosco</h1>
-    </S.Title>  
-
-      <div className = "line"></div>
-
       <div>
-      <input type="text" 
-            placeholder="Nome" 
-            required />
-      </div>
+        <S.Title>
+          <h1>Fale conosco</h1>
+        </S.Title>
 
-      <div>
-      <input type="email" 
-            placeholder="Email" 
-            required />
+        <label className="line"></label>
 
-      </div>
+        <span>Entre em contato</span>
 
-      <div>
-      <textarea id="myTextarea" placeholder='Mensagem'></textarea>
-      </div>
+        <label>
+          <input type="text" placeholder="Nome" required />
+        </label>
 
-      <div>
-      <button type='submit'>Enviar</button>
+        <label>
+          <input type="email" placeholder="Email" required />
+        </label>
+
+        <label>
+          <input type="tel" placeholder="Telefone" required />
+        </label>
+
+        <label>
+          <textarea
+            rows={5}
+            cols={50}
+            id="myTextarea"
+            placeholder="Mensagem"
+          ></textarea>
+        </label>
+
+        <label>
+          <button type="submit">Enviar</button>
+        </label>
+
+        <label>
+          <h6>
+            Este site é protegido por reCAPTCHA. A Política de Privacidade e os
+            Termos de Serviço do Google são aplicáveis.
+          </h6>
+        </label>
+
+        <span>O seu sucesso é a nossa recompensa!!!</span>
+
+        <label>
+          <button type="submit">Enviar</button>
+        </label>
       </div>
-      
     </S.Form>
-  )
-}
+  );
+};
 
 export default Form;
--- src/components/contact/form/styles.ts
+++ src/components/contact/form/styles.ts
@@ -1,15 +1,58 @@
 import styled from 'styled-components'
 
-export const Form = styled.div`
-    display: flex;
-    width: 400px;
-    flex-direction: column;
-    margin: auto;
-    align-items: center;
-    gap:10px;
+export const Form = styled.label`
+    
+    p{
+        font-size: 28px;
+        color: #021F3E;
+        font-family: 'Archivo Black', Arial, sans-serif;
+    }
+
+    div{
+        box-sizing: border-box;
+        display: flex;
+        width: 960px;
+        height: 100vh;
+        flex-direction: column;
+        margin: auto;
+        align-items: center;
+        gap:10px; 
+        background-color: blue;
+        
+    }
+
+    input, textarea {
+        outline: none;
+        padding: 20px;
+        width: 750px;
+        border: 1px solid;
+        border-radius: 10px;
+            
+    }
+
+    input:hover{
+        transition: 0.3s;
+        transform: scale(1.05);
+    }
+
+    textarea:hover{
+        transition: 0.3s;
+        transform: scale(1.05);
+    }
+
+
+   .contato{
+        font-family: Montserrat, Arial, sans-serif;
+        color: #1B1B1B;       
+        font-weight: 1px;
+        font-size: 22px;
+        margin: 40px;
+
+
+   }
 
    .line{
-    border-bottom: 5px solid #021F3E;
+    border-bottom: 2px solid #021F3E;
     width: 200px;
     margin: auto;
    }
@@ -19,12 +62,22 @@ export const Form = styled.div`
     height: 50px;
     background-color: #021F3E;
     color: #FFF;
-    border-radius: 5px;
+    border-radius: 10px;
     font-size: 15px;
+    outline: none;
+    cursor: pointer;
+    
    }
+
+   button:hover{
+    transition: 0.3s;
+    transform: scale(1.1);
+   }
+
+   
 `
 
-export const Title = styled.div`
+export const Title = styled.label`
  
    
     color: #021F3E;
