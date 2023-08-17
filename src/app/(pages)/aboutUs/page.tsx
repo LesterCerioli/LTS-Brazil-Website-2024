125fca0dc4425c4341e2f711cf112365022e43b6
bruno_cassanti <muniz.brunno@gmail.com>
2023-08-17 10:22:53 -0300

    issue(#940): adjusting responsibility

--- src/app/(pages)/aboutUs/page.tsx
+++ src/app/(pages)/aboutUs/page.tsx
@@ -15,7 +15,7 @@ export default function AboutUs() {
 
       <div className="frist-second">
         <S.Section>
-          <Image src={EscritorioImage.src} alt="Time Lucas(imagem ilustrativa)" width={600} height={250} />
+          <Image src={EscritorioImage.src} alt="Time Lucas(imagem ilustrativa)" width={600} height={250} className="img"/>
           <S.SectionContent>
             <p>
             Lucas Tecnologia Serviços. é uma empresa de TI em rápido crescimento que fornece soluções de TI holísticas para empresas em todas as regiões. Oferecemos experiência em TI no desenvolvimento de software, site e aplicativo (iOS e Android); Design de UI/UX; e marketing digital. Estamos situados em cerca de 12 postos de atendimento, atendendo empresas de diversos setores. Até agora, temos incursionado nos setores de saúde, serviços de alimentação, viagens e turismo, logística e transporte, comércio eletrônico, bancos, tecnologia, manufatura e serviços de educação habilitados para TI. Atualmente, temos uma equipe de profissionais de TI altamente qualificados que trabalham 24 horas por dia para garantir que a Techasoft permaneça fiel às suas ofertas e compromissos. Nossa equipe de especialistas experientes inclui desenvolvedores de software, desenvolvedores web, designers e especialistas em marketing digital.
@@ -23,6 +23,7 @@ export default function AboutUs() {
           </S.SectionContent>
         </S.Section>
       </div>
+      
       <S.SecondComponent>
       <div className="second-component">
         <S.SecondTitle>
@@ -35,7 +36,7 @@ export default function AboutUs() {
               Nós, da Lucas Tecnologia Services, acreditamos em trabalhar em parceria com nossos clientes. Cada um de nossos clientes é igualmente importante para nós e colaboramos para entender suas necessidades e métodos de trabalho exclusivos. Nós interagimos e trabalhamos juntos de forma consistente para criar soluções inteligentes que atendam às demandas de negócios em evolução de nossos clientes. E, assim, fornecer retornos rápidos seguindo esse estilo colaborativo de trabalho. Estamos continuamente fazendo a ponte entre negócios e tecnologia. Acreditamos que a comunicação é a chave e nossa equipe de especialistas sempre segue esse princípio. Realizamos uma comunicação constante entre os membros da equipe e os clientes, garantindo a transparência. Isso também nos permite obter maiores insights, bem como a confiança de todos os nossos clientes. Todos os nossos projetos são entregues nos prazos estabelecidos e nossos serviços estão dentro de uma faixa bastante acessível. Nosso processo de desenvolvimento é realizado de forma transparente e segue uma abordagem sistemática – Começamos com a assinatura de um Non-Disclosure Agreement (NDA) com o cliente. Post qual processo e documentação é feito. As responsabilidades são compartilhadas entre os membros da equipe para garantir uma progressão oportuna e suave do projeto. É feito o agendamento adequado de todo o processo e também realizamos relatórios frequentes para manter todos (incluindo os clientes) informados. Temos uma abordagem completamente ágil e obtemos feedback e resultados contínuos do cliente e, consequentemente, mudamos e progredimos com nosso desenvolvimento. É por isso que podemos assumir projetos complexos que outros desenvolvedores podem perceber como difíceis, tornando-nos um dos melhores desenvolvedores de software do mercado.
             </p>
           </S.TextElement>
-          <Image src={LogoImage.src} alt="Descrição da segunda imagem" width={440} height={508} />
+          <Image src={LogoImage.src} alt="Descrição da segunda imagem" width={440} height={508} className="img"/>
         </S.SecondElement>
       </div>
       </S.SecondComponent>
--- src/app/(pages)/aboutUs/styles.ts
+++ src/app/(pages)/aboutUs/styles.ts
@@ -3,84 +3,112 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  aling-items: center;
+  align-items: center;
   justify-content: center;
-}
+  padding: 2rem;
 `;
 
 export const Title = styled.div`
-  
-h1 {
-  display: flex;
-  justify-content: center;
-  font-size: 3.4rem;
-  margin-top: 5rem;
-  color: #021F3E;
-}
-
-.hline {
-  border-bottom: 2px solid lightgray;
-  margin: 0.5rem 44rem;
-  margin-bottom: 3rem;
-}
-
-@media screen and (max-width: 600px) {
-  width: 80%;
   h1 {
-    font-size: 30px;
+    display: flex;
+    justify-content: center;
+    font-size: 3.4rem;
+    margin-top: 3rem;
+    color: #021F3E;
+  }
+
+  .hline {
+    border-bottom: 2px solid lightgray;
+    margin: 0.5rem auto;
+    margin-bottom: 2rem;
+  }
+
+  @media screen and (max-width: 600px) {
+    width: 100%;
+
+    h1 {
+      font-size: 30px;
+    }
   }
 `;
 
 export const Section = styled.div`
   display: flex;
-  justify-content: center;
-  margin: 3rem 6rem;
-  gap: 6rem;
+  flex-direction: column;
+  align-items: center;
+  margin: 2rem 0;
 
-  @media screen and (max-width: 600px) {
-    flex-direction: column;
-
-    img {
-      height: auto;
-    }
+  .img {
+    width: 100%;
+    height: auto;
   }
 `;
 
 export const SectionContent = styled.div`
   font-size: 20px;
   font-family: 'Roboto', sans-serif;
-  margin-bottom: 4rem;
+  margin-bottom: 2rem;
+  max-width: 800px;
+
+  @media screen and (max-width: 600px) {
+    font-size: 18px;
+  }
 `;
 
 export const SecondComponent = styled.div`
   background-color: #F6F6F6;
-`
+`;
 
 export const SecondTitle = styled.div`
-h1 {
-  display: flex;
-  justify-content: center;
-  font-size: 3.4rem;
-  margin-top: 5rem;
-  color: #021F3E;
-}
+  h1 {
+    display: flex;
+    justify-content: center;
+    font-size: 3.4rem;
+    margin-top: 3rem;
+    color: #021F3E;
+  }
 
-.htowline {
-  border-bottom: 2px solid lightgray;
-  margin: 0.5rem 36rem;
-  margin-bottom: 4rem;
-}
-`
+  .htowline {
+    border-bottom: 2px solid lightgray;
+    margin: 0.5rem auto;
+    margin-bottom: 2rem;
+  }
+
+  @media screen and (max-width: 600px) {
+    width: 100%;
+    text-align: center;
+
+    .img {
+      width: 100%;
+      height: auto;
+    }
+
+    h1 {
+      font-size: 30px;
+    }
+
+    margin: 1rem 0;
+  }
+`;
 
 export const SecondElement = styled.div`
   display: flex;
-  justify-content: center;
-  margin: 6rem 6rem;
-  gap: 6rem;
+  flex-direction: column;
+  align-items: center;
+  margin: 2rem 0;
+  gap: 2rem;
+
+  @media screen and (min-width: 601px) {
+    flex-direction: row;
+  }
 `;
 
 export const TextElement = styled.div`
   font-size: 20px;
   font-family: 'Roboto', sans-serif;
-`;
+  max-width: 800px;
 
+  @media screen and (max-width: 600px) {
+    font-size: 18px;
+  }
+`;
