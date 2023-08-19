041d2aba87f1faaf187ff98e0b7f6a2b46d8e85d
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2023-08-19 01:32:47 +0000

    Revert "Merged PR 419: issue(#934): finished home page
    issue(#934): finished home page"

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -1,10 +1,7 @@
 "use client";
-import React, { useRef } from "react";
+import React from "react";
 import Image from "next/image";
 import * as S from "./styles";
-import Link from "next/link";
-import { Cookies } from "@/components/home/cookies/cookies";
-
 import CapaEnglosite from "@/assets/imagesHome/capaEnglosite.png";
 import Design from "@/assets/imagesHome/design.png";
 import Crms from "@/assets/imagesHome/crms.png";
@@ -19,54 +16,19 @@ import Hospitalar from "@/assets/imagesHome/hospitalar.png";
 import Turismo from "@/assets/imagesHome/turismo.png";
 import Educacao from "@/assets/imagesHome/educacao.png";
 import CloudSuite from "@/assets/imagesHome/cloudSuite.png";
-import Chevron from "@/assets/imagesHome/right_chevron_icon.png";
 
 export default function Home() {
-  const carousel = useRef<HTMLDivElement | null>(null);
-
-  const handleLeftClick = (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    e.preventDefault();
-    if (carousel.current) {
-      carousel.current.scrollLeft -= carousel.current.offsetWidth;
-    }
-  };
-
-  const handleRightClick = (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    e.preventDefault();
-    if (carousel.current) {
-      carousel.current.scrollLeft += carousel.current.offsetWidth;
-    }
-  };
-
   return (
     <S.Container>
       <S.Title>
-        <div className="containerTitle">
-          <S.TextTitle>
-            <h1>Bem-vindo(a) a Lucas Tecnologia Services!</h1>
-            <p>A mudança do seu negócio começa aqui.</p>
-          </S.TextTitle>
-        </div>
-        <video
-          data-aid="HEADER_VIDEO"
-          type="video/mp4"
-          poster="https://img1.wsimg.com/isteam/videos/QzGPEQx"
-          autoplay=""
-          loop="true"
-          muted="true"
-          playsinline=""
-          src="https://categories.api.godaddy.com/v4/videos/raw/video/QzGPEQx"
-        ></video>
+        <h1>Bem-vindo(a) a Lucas Tecnologia Services!</h1>
+        <p>A mudança do seu negócio começa aqui.</p>
       </S.Title>
 
       <div>
-        <S.StartHere>
+        <S.FirstSection>
           <S.SectionStart>
-            <Image src={CapaEnglosite} alt="Comece aqui" />
+            <Image src={CapaEnglosite} alt="" />
           </S.SectionStart>
 
           <S.SectionStart>
@@ -77,156 +39,122 @@ export default function Home() {
               e prototipagem, desenvolvimento de software, teste, implantação e
               manutenção de aplicativos.
             </p>
-
-            <Link href="/services">
-              <button>Saiba mais</button>
-            </Link>
+            <button>Saiba mais</button>
           </S.SectionStart>
-        </S.StartHere>
+        </S.FirstSection>
 
-        <S.OurServices>
+        <S.SecondSection>
           <div>
             <h2>Nossos Serviços</h2>
             <div className="hLine"></div>
           </div>
-          <S.SectionCards ref={carousel}>
-            <Link href="/services">
-              <div className="card">
-                <div className="textCard">
-                  <h3>UI/UX Design</h3>
-                  <p>
-                    Aumenta a experiência do usuário e a satisfação do cliente,
-                    resultando no crescimento do negócio.
-                  </p>
-                </div>
-
-                <Image src={Design} alt="design" />
+          <S.SectionCards>
+            <div className="card">
+              <div className="textCard">
+                <h3>UI/UX Design</h3>
+                <p>
+                  Aumenta a experiência do usuário e a satisfação do cliente,
+                  resultando no crescimento do negócio.
+                </p>
               </div>
-            </Link>
 
-            <Link href="/services">
-              <div className="card">
-                <div className="textCard">
-                  <h3>Marketing Digital</h3>
-                  <p>
-                    Somos uma empresa de marketing digital avançada no Brasil.
-                    Criamos estratégias para divulgar sua marca, facilitando a
-                    interação
-                  </p>
-                </div>
-
-                <Image src={Marketing} alt="marketing" />
+              <Image src={Design} alt="design" />
+            </div>
+            <div className="card">
+              <div className="textCard">
+                <h3>Marketing Digital</h3>
+                <p>
+                  Somos uma empresa de marketing digital avançada no Brasil.
+                  Criamos estratégias para divulgar sua marca, facilitando a
+                  interação
+                </p>
               </div>
-            </Link>
 
-            <Link href="/services">
-              <div className="card">
-                <div className="textCard">
-                  <h3>Desenvolvimento de CRMs</h3>
-                  <p>
-                    É a ferramenta ou técnica que ajuda as empresas a fazer um
-                    relacionamento saudável com seus clientes, organizando os
-                    dados.
-                  </p>
-                </div>
-
-                <Image src={Crms} alt="crms" />
+              <Image src={Marketing} alt="marketing" />
+            </div>
+            <div className="card">
+              <div className="textCard">
+                <h3>Desenvolvimento de CRMs</h3>
+                <p>
+                  É a ferramenta ou técnica que ajuda as empresas a fazer um
+                  relacionamento saudável com seus clientes, organizando os
+                  dados.
+                </p>
               </div>
-            </Link>
 
-            <Link href="/services">
-              <div className="card">
-                <div className="textCard">
-                  <h3>Desenvolvimento de sites</h3>
-                  <p>
-                    Envolve a construção e manutenção dos sites. Fuinciona
-                    rapidamente com uma experiência de usuário firme.
-                  </p>
-                </div>
-
-                <Image src={Sites} alt="sites" />
+              <Image src={Crms} alt="crms" />
+            </div>
+            <div className="card">
+              <div className="textCard">
+                <h3>Desenvolvimento de sites</h3>
+                <p>
+                  Envolve a construção e manutenção dos sites. Fuinciona
+                  rapidamente com uma experiência de usuário firme.
+                </p>
               </div>
-            </Link>
 
-            <Link href="/services">
-              <div className="card">
-                <div className="textCard">
-                  <h3>Desenvolvimento mobile</h3>
-                  <p>
-                    É o ato de desenvolver aplicativos adequados para
-                    dispositivos móveis que envolvem a escrita de software para
-                    os mesmos.
-                  </p>
-                </div>
-
-                <Image src={Mobile} alt="mobile" />
+              <Image src={Sites} alt="sites" />
+            </div>
+            <div className="card">
+              <div className="textCard">
+                <h3>Desenvolvimento mobile</h3>
+                <p>
+                  É o ato de desenvolver aplicativos adequados para dispositivos
+                  móveis que envolvem a escrita de software para os mesmos.
+                </p>{" "}
               </div>
-            </Link>
+
+              <Image src={Mobile} alt="mobile" />
+            </div>
           </S.SectionCards>
+        </S.SecondSection>
 
-          <S.ButtonsCarousel>
-            <button onClick={handleLeftClick}>
-              <Image src={Chevron} alt="Scroll Left" />
-            </button>
-
-            <button onClick={handleRightClick}>
-              <Image src={Chevron} alt="Scroll Left" />
-            </button>
-          </S.ButtonsCarousel>
-        </S.OurServices>
-
-        <S.SiteEua>
-          <Image src={Eua} alt="Site EUA" />
+        <S.ThirdSection>
+          <Image src={Eua} alt="" />
           <div>
             <h2> Lucas Tecnologia nos Estados Unidos</h2>
-            <Link href="https://lucastechnologyservice.com/">
-              <button> Saiba mais</button>
-            </Link>
+            <button> Saiba mais</button>
           </div>
-        </S.SiteEua>
+        </S.ThirdSection>
 
-        <S.AreasOfExpertise>
+        <S.FourthSection>
           <h2>Áreas de Atuação</h2>
           <div className="hLine"></div>
 
-          <S.AreasOfExpertiseCards>
+          <S.FourthSectionCards>
             <S.Card>
               <h3>Tecnologia</h3>
-              <Image src={Tecnologia} alt="Tecnologia" />
+              <Image src={Tecnologia} alt="" />
             </S.Card>
             <S.Card>
               <h3>E-commerces</h3>
-              <Image src={Ecommerces} alt="E-commerces" />
+              <Image src={Ecommerces} alt="" />
             </S.Card>
             <S.Card>
               <h3>Mercado Financeiro</h3>
-              <Image src={MercadoFinanceiro} alt="Mercado Financeiro" />
+              <Image src={MercadoFinanceiro} alt="" />
             </S.Card>
             <S.Card>
               <h3>Hospitalar</h3>
-              <Image src={Hospitalar} alt="Hospitalar" />
+              <Image src={Hospitalar} alt="" />
             </S.Card>
             <S.Card>
               <h3>Turismo</h3>
-              <Image src={Turismo} alt="Turismo" />
+              <Image src={Turismo} alt="" />
             </S.Card>
             <S.Card>
               <h3>Educação</h3>
-              <Image src={Educacao} alt="Educação" />
+              <Image src={Educacao} alt="" />
             </S.Card>
-          </S.AreasOfExpertiseCards>
+          </S.FourthSectionCards>
 
-          <S.CloudSuite>
-            <Image src={CloudSuite} alt="Cloud Suite" />
+          <S.FullWidthCard>
+            <Image src={CloudSuite} alt="" />
             <h2>Gestão da sua empresa nas suas mãos</h2>
             <div className="hLine"></div>
-          </S.CloudSuite>
-        </S.AreasOfExpertise>
+          </S.FullWidthCard>
+        </S.FourthSection>
       </div>
-
-      <S.Cokkies>
-        <Cookies />
-      </S.Cokkies>
     </S.Container>
   );
 }
