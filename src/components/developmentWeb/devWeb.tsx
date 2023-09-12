fc6777bf7427887a86b24c4ca4878da32eeeb2d4
DouglasRibeiro1972 <douglascri@yahoo.com.br>
2023-09-12 11:24:26 -0300

    feature(#1016): Development Web page finished

--- src/components/developmentWeb/devWeb.tsx
+++ src/components/developmentWeb/devWeb.tsx
@@ -1,52 +1,47 @@
 'use client';
 import React from 'react';
-import Image from 'next/image';
 import * as S from "./styles";
-import devWeb from '@/assets/imagesDevWeb/services_hero.svg'
-import { Container } from 'react-bootstrap';
 
 export default function DevelopmentWeb() {
   return (
-    <S.Container>
-      
-      <S.Title>
+    <S.Container >
+       
+       <S.Mission>
+         <S.Title> 
           Desenvolvimento Web
+         </S.Title>
         
-      </S.Title>
-
-      <S.Mission>
-        
-           Desenvolvimento de Sites Profissionais para o Sucesso Online, onde  transformamos ideias em presença online notável. Somos uma equipe de especialistas dedicados a criar soluções digitais que impressionam e geram resultados. Combinando design atraente e funcionalidades avançadas, oferecemos uma variedade de serviços personalizados para atender às suas necessidades.        
+        <S.Subtitle>
+          Desenvolvimento de Sites Profissionais para o Sucesso Online, onde  transformamos ideias em presença online notável. Somos uma equipe de especialistas dedicados a criar soluções digitais que impressionam e geram resultados. Combinando design atraente e funcionalidades avançadas, oferecemos uma variedade de serviços personalizados para atender às suas necessidades.
+        </S.Subtitle>        
         
       </S.Mission>
-
+      
       <S.Imag>
-        
-          <Image src={devWeb} alt='devWeb' width={1074} height={437}/>
+        <S.Img src='/assets/imagesDevWeb/services_hero.svg'></S.Img>
       </S.Imag>
-
+ 
       <S.Resources>
         <S.Itens>
-          <div>
-            
+          
+          <S.Card>            
             Sites modernos, atraentes, otimizados para funcionar perfeitamente em todos os dispositivos, garantindo a melhor experiência do usuário.
-            
-          </div>
+          </S.Card>
 
-          <div >
-           Transforme sua ideia de negócio em uma loja online de sucesso. Oferecemos soluções de comércio eletrônico personalizadas para impulsionar suas vendas online.        
-          </div>
+          <S.Card >  
+          Transforme sua ideia de negócio em uma loja online de sucesso. Oferecemos soluções de comércio eletrônico personalizadas para impulsionar suas vendas online.        
+          </S.Card>
         
-          <div>
+          <S.Card>
             Garantimos que seu site seja rápido e eficiente, melhorando sua classificação nos mecanismos de busca e a satisfação do cliente. 
-          </div>
+          </S.Card>
 
-          <div>
+          <S.Card>
             Simplificamos a gestão do seu site com a integração de sistemas de gerenciamento de conteúdo (CMS) robustos, permitindo que você atualize facilmente seu conteúdo.        
-          </div>
-        </S.Itens>
-      </S.Resources>
+          </S.Card>
 
+        </S.Itens>
+      </S.Resources>    
     </S.Container>
   )
 }
