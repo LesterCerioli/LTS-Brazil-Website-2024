ab4350adecfca8e168f392856106382a4f8ee825
DouglasRibeiro1972 <douglascri@yahoo.com.br>
2023-09-15 11:22:42 -0300

    feature(#1016): Development Web page margin-top adequacy

--- src/components/developmentWeb/styles.ts
+++ src/components/developmentWeb/styles.ts
@@ -55,7 +55,7 @@ export const Title = styled.h1`
   font-size: 48px;
   line-height: 1.5;
   letter-spacing: 0.025em;
-  padding-top: 20px;
+  padding-top: 60px;
   margin-bottom: 15px;
   width: 90%;
   color: #000;
