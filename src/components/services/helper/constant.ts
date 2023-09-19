e0bfe6f690e2bd90645c5d703d59e388514bf6c8
DouglasRibeiro1972 <douglascri@yahoo.com.br>
2023-09-18 22:52:18 -0300

    feature(#1122): Services Pages initial

---
+++ src/components/services/helper/constant.ts
@@ -0,0 +1,79 @@
+//importar as imagens
+// import img_crmservices from xxxx jogar o alias em image
+//criar os outros objetos como o exempo abaixo
+//improtomar o botão
+
+import imgServicesHero from '@/assets/imagesServices/services_hero.svg';
+import imgMobile from '@/assets/imagesServices/Thumb_mobile.svg';
+
+
+
+export const servicesPageData = [
+    {
+        id:1, 
+        title:  'Desenvolvimento Web de portais para restaurante',
+        image: 'imgServicesHero',
+        link: ' ',
+        
+    },
+
+    {
+        id:2, 
+        title:  'Desenvolvimento Mobile',
+        image: 'imgMobile',
+        link: ' ',
+        
+        
+    },
+
+    {
+        id:3, 
+        title:  'Serviços Educacionais',
+        image: 'image',
+        link: ' ',
+        
+    },
+
+    {
+        id:4, 
+        title:  'Educational Services',
+        image: 'image',
+        link: ' ',
+        
+    },
+
+    {
+        id:5, 
+        title:  'Services',
+        image: 'image',
+        link: ' ',
+    },
+
+    {
+        id:6, 
+        title:  'Real State',
+        image: 'image',
+        link: ' ',
+        subtitle: 'subtitle',
+        
+    },
+
+    {
+        id:7, 
+        title:  'Travel Services',
+        image: 'image',
+        link: ' ',
+        
+    },
+
+    {
+        id:8, 
+        title:  'title',
+        image: 'image',
+        link: ' ',
+        
+        
+    },
+
+   
+]
