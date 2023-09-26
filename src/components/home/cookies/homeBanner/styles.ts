19f613d90bbbbf05ef687440aa23ce747319d3ed
Luiz Guilherme Reis <luixguilherme@hotmail.com>
2023-09-26 13:43:47 -0300

    feature(#1121): Home page finished

--- src/components/home/cookies/homeBanner/styles.ts
+++ src/components/home/cookies/homeBanner/styles.ts
@@ -7,6 +7,36 @@ background: url('/assets/imagesHome/LTS_header_banner.svg') no-repeat ;
 background-size: cover;
 background-position: center;
 
+@media (min-width: 1300px) and (max-width:1400px) {
+   background: url('/assets/imagesHome/background_banner.svg') no-repeat;
+   width: 100%;
+   max-height: 1000px;
+}
+
+@media (min-width: 1250px) and (max-width:1299px) {
+   background: url('/assets/imagesHome/background_banner.svg') no-repeat;
+   width: 100%;
+   max-height: 750px;
+}
+
+@media (min-width: 1100px) and (max-width:1249px) {
+   background: url('/assets/imagesHome/background_banner.svg') no-repeat;
+   width: 100%;
+   max-height: 700px;
+}
+
+@media (min-width: 1000px) and (max-width:1099px) {
+   background: url('/assets/imagesHome/background_banner.svg') no-repeat;
+   width: 100%;
+   max-height: 620px;
+}
+
+@media (min-width: 891px) and (max-width: 999px) {
+   background: url('/assets/imagesHome/background_banner.svg') no-repeat;
+   width: 100%;
+   max-height: 560px;
+}
+
 @media (min-width: 800px) and (max-width:890px) {
    background: url('/assets/imagesHome/background_banner.svg') no-repeat;
    width: 100%;
@@ -77,7 +107,7 @@ background-position: center;
 export const Logo = styled.img`
    display: none;
 
-   @media (max-width: 890px) {
+   @media (max-width: 1400px) {
       display: flex;
       max-width: 70%;
       max-height: 70%;
