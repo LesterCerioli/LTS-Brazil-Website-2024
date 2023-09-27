94e9c03815e7f5a0e2b0d6618af362ae9ee25cc3
artprange <arthur.prange@gmail.com>
2023-09-27 11:39:11 -0300

    feat(#1151): DO NOT TOUCH! test environment!

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,6 @@
-// import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
-// export default defineConfig({
-//   e2e: {
-//     setupNodeEvents(on, config) {
-//       // implement node event listeners here
-//     },
-//   },
-// });
-
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
   },
