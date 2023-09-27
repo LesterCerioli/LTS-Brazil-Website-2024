753caff30dc018b6faa16745400e11faf13c1020
artprange <arthur.prange@gmail.com>
2023-09-27 14:01:46 -0300

    feat(#1151): creating new test branch: implementing jest --saving

--- src/components/blog/postsPage/Posts.tes.tsx
+++ src/components/blog/postsPage/Posts.tes.tsx
@@ -2,16 +2,19 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import Posts from './Posts'
-import Image from 'next/image'
 
-// // Explicitly type the jest.mock call
+import Posts from './Posts'
+
+// Explicitly type the jest.mock call
 // jest.mock(
 //   'next/image',
 //   () =>
 //     ({ src, alt }: { src: string; alt: string }) =>
-//       (<Image src={src} alt={alt} />) as React.FC
+//       (<img src={src} alt={alt} />) as React.FC<{}>
 // )
+
+// Explicit type definition for shuffleArray
+type ShuffleArray = <T>(array: T[]) => T[]
 
 // Mocking the postCategoryImages array
 jest.mock('@/components/blog/helper/postCategotyImages', () => ({
@@ -24,28 +27,13 @@ jest.mock('@/components/blog/helper/postCategotyImages', () => ({
 
 describe('Posts Component', () => {
   it('renders posts with shuffled images and links', () => {
+    // Mock the shuffleArray function with the explicit type
+    jest
+      .spyOn(global, 'shuffleArray' as keyof ShuffleArray)
+      .mockReturnValue(mockedImages)
+
     render(<Posts />)
 
-    // Assert that the component renders
-    const headerElement = screen.getByText(/Confira nossos posts/i)
-    expect(headerElement).toBeInTheDocument()
-
-    // Mocked data for postCategoryImages
-    const mockedImages = [
-      { image: 'mock-image-1.jpg', link: 'mock-link-1' },
-      { image: 'mock-image-2.jpg', link: 'mock-link-2' },
-    ]
-
-    // Mock the shuffleArray function to return the mockedImages array
-    jest.spyOn(global, 'shuffleArray').mockReturnValue(mockedImages)
-
-    // Assert that the images are rendered with the correct alt text and links
-    mockedImages.forEach((item, index) => {
-      const imageElement = screen.getByAltText(`Image ${index}`)
-      const linkElement = screen.getByRole('link', { name: `Image ${index}` })
-
-      expect(imageElement).toHaveAttribute('src', item.image)
-      expect(linkElement).toHaveAttribute('href', item.link)
-    })
+    // ... (rest of your test)
   })
 })
