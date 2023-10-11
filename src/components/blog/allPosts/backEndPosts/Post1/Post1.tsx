1fa203df28bdb537aeafe7eca03a140ce480f2d6
artprange <arthur.prange@gmail.com>
2023-10-11 14:30:00 -0300

    bug(#1153): making progress

--- src/components/blog/allPosts/backEndPosts/Post1/Post1.tsx
+++ src/components/blog/allPosts/backEndPosts/Post1/Post1.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import * as S from "./styles";
-import Image from "next/image";
-import img from "@/assets/imagesBlog/posts/backEndPostsImages/post1.png";
+import React from 'react'
+import * as S from './styles'
+import Image from 'next/image'
+import img from '@/assets/imagesBlog/posts/backEndPostsImages/post1.png'
 
 function Post1() {
   return (
@@ -10,12 +10,9 @@ function Post1() {
         <S.TextContainer>
           <div>
             <h1>Comparando Java e Kotlin: Pros and Cons</h1>
-            <Image
-              src={img}
-              alt="java vs. kotlin"
-              width={800}
-              className="img"
-            />
+            <S.Img>
+              <Image src={img} alt="java vs. kotlin" />
+            </S.Img>
             <h4>
               Introdução:Java e Kotlin são duas linguagens de programação
               populares que são frequentemente usadas no desenvolvimento de
@@ -35,12 +32,12 @@ function Post1() {
               muito grande e ativa, o que significa que há muita documentação,
               bibliotecas e recursos disponíveis.
             </p>
-            <br></br>{" "}
+            <br></br>{' '}
             <p>
               Multiplataforma: Java é conhecido por sua capacidade de executar
               em diversas plataformas, tornando-o uma escolha sólida para
               desenvolvimento não apenas em Android, mas também em servidores e
-              outras aplicações.{" "}
+              outras aplicações.{' '}
             </p>
             <br></br>
             <p>
@@ -55,43 +52,43 @@ function Post1() {
             <p>
               Verbosidade: Java é uma linguagem mais verbose em comparação com
               Kotlin. Isso significa que você precisa escrever mais código para
-              realizar tarefas simples.{" "}
+              realizar tarefas simples.{' '}
             </p>
             <br></br>
             <p>
               Boilerplate Code: Em Java, há muita necessidade de escrever código
               boilerplate para realizar tarefas que seriam mais simples em
-              outras linguagens.{" "}
+              outras linguagens.{' '}
             </p>
             <br></br>
             <h2>Vantagens do Kotlin:</h2>
             <p>
               Conciso e Expressivo: Kotlin é conhecido por sua sintaxe concisa e
               expressiva, o que permite escrever menos código e torna o código
-              mais legível.{" "}
+              mais legível.{' '}
             </p>
             <br></br>
             <p>
               Segurança de Tipos: Kotlin oferece segurança de tipos aprimorada
               em comparação com Java, reduzindo a probabilidade de erros em
-              tempo de execução.{" "}
+              tempo de execução.{' '}
             </p>
             <br></br>
             <p>
               Interoperabilidade: Kotlin é 100% interoperável com Java, o que
               significa que você pode facilmente usar bibliotecas Java em
-              projetos Kotlin e vice-versa.{" "}
+              projetos Kotlin e vice-versa.{' '}
             </p>
             <br></br>
             <p>
               Nulabilidade Controlada: Kotlin introduz o conceito de tipos de
               dados nulos, o que ajuda a evitar muitos erros de referência nula
-              que são comuns em Java.{" "}
+              que são comuns em Java.{' '}
             </p>
             <br></br>
             <h2> Desvantagens do Kotlin:</h2>
             <p>
-              {" "}
+              {' '}
               Curva de Aprendizado: Embora Kotlin seja considerado fácil de
               aprender para desenvolvedores Java, pode haver uma curva de
               aprendizado inicial. <br></br>
@@ -99,11 +96,11 @@ function Post1() {
             <p>
               Menos Material de Aprendizado: Embora a comunidade Kotlin esteja
               crescendo rapidamente, ainda pode haver menos material de
-              aprendizado disponível em comparação com Java.{" "}
+              aprendizado disponível em comparação com Java.{' '}
             </p>
             <br></br>
             <h3>
-              {" "}
+              {' '}
               Conclusão: Ambas Java e Kotlin têm seus próprios pontos fortes e
               fracos. A escolha entre as duas depende dos requisitos específicos
               do seu projeto e da sua familiaridade com a linguagem. Enquanto
@@ -117,7 +114,7 @@ function Post1() {
         <div></div>
       </div>
     </S.PostStylesMainContainer>
-  );
+  )
 }
 
-export default Post1;
+export default Post1
--- src/components/blog/allPosts/backEndPosts/Post1/styles.ts
+++ src/components/blog/allPosts/backEndPosts/Post1/styles.ts
@@ -12,22 +12,34 @@ export const PostStylesMainContainer = styled.div`
   background-color: lightgray;
   box-shadow: 2px 2px 10px 10px rgba(0, 0, 0, 0.25);
 
+  @media (max-width: 800px) {
+    .postHero {
+      max-width: 800px;
+    }
+  }
+
   @media (max-width: 390px) {
     max-width: 390px;
     margin-left: 0px;
-    .img {
-      max-width: 350px;
-      max-height: 150px;
+
+    .postHero {
+      display: none;
     }
   }
   @media (max-width: 320px) {
-    max-width: 320px;
-    margin-left: 0px;
-    .img {
+    width: 320px;
+
+    .postHero {
       max-width: 300px;
       height: 150px;
+      margin-right: 50px;
+      display: none;
     }
   }
+`
+
+export const Img = styled.img`
+  max-width: 100%;
 `
 
 export const TextContainer = styled.div`
@@ -63,17 +75,19 @@ export const TextContainer = styled.div`
     max-width: 380px;
   }
   @media (max-width: 320px) {
-    max-width: 320px;
-    margin-right: 15px;
-    h1,
-    h2,
-    h3,
-    h4 {
-      max-width: 300px;
-      margin-top: 45px;
-      margin-bottom: 45px;
-      letter-spacing: 1px;
-      line-height: 25px;
+    div {
+      width: 300px;
+      margin-left: -60px;
+      h1,
+      h2,
+      h3,
+      h4 {
+        max-width: 320px;
+        margin-top: 45px;
+        margin-bottom: 45px;
+        letter-spacing: 1px;
+        line-height: 25px;
+      }
     }
   }
 `
