8d449a100a81f33b4d16b6ccc81e00d9d2a0396e
Lester Cerioli <lesterlucasit@hotmail.com>
2024-10-29 17:19:26 -0400

    feat(#14): Fixing bug

---
+++ src/app/(pages)/admin/it/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+import * as S from "./styles";
+import { Banner } from "@/components/home/cookies/homeBanner/homeBanner";
+
+const IT: React.FC = () => {
+    return (
+        <S.Container>
+            
+
+        </S.Container>
+
+    )
+
+}
+export default IT;
